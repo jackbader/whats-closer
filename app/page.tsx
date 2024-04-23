9f6d13a70201f3f68be118a671a0f5dc1597928a
Jack Bader <jack@culpritunderwear.com>
2024-04-23 03:15:01 -0400

    fixing dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { NextUIProvider } from "@nextui-org/react";
 export default function Home() {
   return (
     <NextUIProvider>
-      <main className="dark flex min-h-screen flex-col items-center p-6">
+      <main className="dark text-foreground bg-background flex min-h-screen flex-col items-center p-6">
         <h1 className="text-4xl py-4">Whats Closer?</h1>
         <AddressForm />
       </main>
